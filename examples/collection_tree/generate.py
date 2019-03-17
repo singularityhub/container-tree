061aa3f7ad00dace3ae7383e9142641469a37f4e
Vanessa Sochat <vsochat@stanford.edu>
2019-03-17 15:33:11 -0400

    adding final tree

--- examples/collection_tree/generate.py
+++ examples/collection_tree/generate.py
@@ -80,3 +80,11 @@ else:
 
     # Save final tree
     pickle.dump(tree, open('container-collection-tree.pkl','wb'))
+
+
+    # Now add nodes onto library
+    for pair in pairs:
+        tree.update(uri=pair[0], fromuri=pair[1])
+
+    # Save final tree
+    pickle.dump(tree, open('container-collection-tree.pkl','wb'))
